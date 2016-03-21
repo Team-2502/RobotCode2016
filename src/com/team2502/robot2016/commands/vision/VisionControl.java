7f64e982e7326b43214e9931fe10e6bb23ba400b
vandench <64006628@hs-54922-macair.ad.edenpr.org>
2016-03-21 11:54:58 -0500

    Revert "Remove vision code."
    This reverts commit 623a02ac0e32633f3955dabb2c4daedb5ba6a606.

---
+++ src/com/team2502/robot2016/commands/vision/VisionControl.java
@@ -0,0 +1,43 @@
+package com.team2502.robot2016.commands.vision;
+
+import com.team2502.robot2016.Robot;
+import com.team2502.robot2016.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class VisionControl extends Command {
+	
+//	private Subsystem dt = Robot.vision;
+	
+	public VisionControl()
+	{
+//		requires(Robot.vision);
+	}
+	
+	@Override
+	protected void initialize() {
+		
+	}
+
+	@Override
+	protected void execute() {
+//		int value = Robot.serialPort.getBytesReceived();
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		
+	}
+}
---
+++ src/com/team2502/robot2016/subsystems/Vision.java
@@ -0,0 +1,23 @@
+package com.team2502.robot2016.subsystems;
+
+import com.team2502.robot2016.commands.vision.VisionControl;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Parity;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.SerialPort.StopBits;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Vision extends Subsystem {
+
+	/**
+	 *  For communication between RaspberryPi (Vision) and RoboRio
+	 */
+	public static final SerialPort serialPort = new SerialPort(9600, Port.kOnboard, 8, Parity.kNone, StopBits.kOne);
+	
+	
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(new VisionControl());
+	}
+}
